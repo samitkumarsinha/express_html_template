3eecbea5a457997a11d33786e4fe848c07dbc96f
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 16:32:49 +0530

    init

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ var port = process.env.PORT || 3000;
 
 http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.end('Time now ' + dt.mydate());
+    res.write('ok');
+    res.end('Time now >> ' + dt.mydate());
+    
 }).listen(port);
