26ca9820329e3c7686ce7b5646c940112d85c369
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 15:55:32 +0530

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var port = process.env.PORT || 3000;
+var port = process.env.port || 3000;
 
 http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain' });
