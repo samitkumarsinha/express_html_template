c2f240b355e74add2856cf11d0654c6d019a8391
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 20:38:21 +0530

    init

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 var http = require('http');
-var dt = require('./datemodule');
+// var dt = require('./datemodule');
 var port = process.env.PORT || 3000;
+var nmail = require('nodemailer');
+const SendmailTransport = require('nodemailer/lib/sendmail-transport');
 
+send_mail(res){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'samitkumarsinhas@gmail.com',
+          pass: 'bishalbadribaba'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'samitkumarsinhas@gmail.com',
+        to: 'samitkumarsinha@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          res.end('Error');
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/plain'});
+          res.end('Email sent: ' + info.response);
+        }
+      });
+}
 http.createServer((req, res)=> {
     res.writeHead(200, {'Content-Type': 'text/plain' });
-    res.write(req.url);
-    res.end('Time now >> ' + dt.mydate());
+    send_mail(res);
     
 }).listen(port);
