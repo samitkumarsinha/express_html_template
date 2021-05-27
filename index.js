9cbb5e3b1df81ee975effc403e69777fda8437a6
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-27 14:49:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://:samdb@cluster0.idow2.mongodb.net/samnode";
+function getData(res){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("school");
+        dbo.collection("users").find({}).limit(2).toArray(function(err, result) {
+        if (err) throw err;
+        res.end(JSON.stringify(result));
+        db.close();
+        });
+    });
+}
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    getData(res);
+}).listen(port);
