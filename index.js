d9f7c457b5891e71e2ecf4137c7248ca29be33da
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-29 13:28:23 +0530

    express

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
 var path = require ('path');
-// app.use('/static', express.static(path.join(__dirname, 'views')))
-
+app.use(express.static(path.join(__dirname, '/views')))
+app.use(express.static(path.join(__dirname, '/images')))
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-
    res.sendFile(__dirname + '/views/template.html');
-
 });
 
 var server = app.listen(port, function () {
