d78e62e5217891a5137e90c2714c46d9d0befdd8
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-29 14:22:42 +0530

    express

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ var path = require ('path');
 app.use(express.static(path.join(__dirname, '/views')))
 app.use(express.static(path.join(__dirname, '/images')))
 var port = process.env.PORT || 3000;
+var arr= [
+   {'name':'WHO'},{'name':'WHAT'},{'name':'WHERE'}
+]
 
 app.get('/', function (req, res) {
-   res.render(__dirname + '/views/template.html', {'port':port});
+   res.render(__dirname + '/views/template.html', {'port':port, 'items': arr},);
 });
 
 var server = app.listen(port, function () {
+   for(let item of arr) console.log(item.name)
    console.log('Node server is running..');
 });
