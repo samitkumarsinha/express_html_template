89e2e1bb613576e10685ca27f279dca80e1ada23
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-30 08:44:37 +0530

    add files

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ var app = express();
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 var path = require ('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/views')))
 app.use(express.static(path.join(__dirname, '/images')))
 var port = process.env.PORT || 3000;
 var arr= [
-   {'name':'WHO'},{'name':'WHAT'},{'name':'WHERE..'}
+   {'name':'WHO'},{'name':'WHAT'},{'name':'Login'}
 ]
 
 app.get('/', function (req, res) {
    res.render(__dirname + '/views/template.html', {'port':port, 'items': arr},);
 });
-
+app.get('/login', function (req, res) {
+   res.render(__dirname + '/views/login.html', {'port':port, 'items': arr},);
+});
+app.post('/submit', function (req, res) {
+   console.log(req.body);
+   res.send("recieved your request!");
+});
 var server = app.listen(port, function () {
    for(let item of arr) console.log(item.name)
    console.log('Node server is running..');
