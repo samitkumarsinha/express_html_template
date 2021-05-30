4f0dbe532cb2fa763543abc0799b319dab5e5362
samitkumarsinha <samitkumarsinha@gmail.com>
2021-05-30 12:16:08 +0530

    add files

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ app.set('view engine', 'html');
 var path = require ('path');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/views')))
-app.use(express.static(path.join(__dirname, '/images')))
+app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, '/images')));
 var port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+var uri = 'mongodb+srv://samdb:unicorn1@cluster0.idow2.mongodb.net/school';
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+var userschema = new mongoose.Schema({
+   username: String,
+   password: String,
+   email: String,
+   role: String
+}
+);
+mongoose.connection.on('connected', (err, res) => {
+   console.log('connnnnnn');
+})
+
+
 var arr= [
    {'name':'WHO'},{'name':'WHAT'},{'name':'Login'}
 ]
@@ -20,6 +35,10 @@ app.get('/login', function (req, res) {
 });
 app.post('/submit', function (req, res) {
    console.log(req.body);
+   var user = mongoose.model('users', userschema);
+   user.find({}, function(err, data){
+      console.log(data[0].email);
+   });
    res.send("recieved your request!");
 });
 var server = app.listen(port, function () {
